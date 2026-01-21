28de4a2c14aaf1cb677e336c355ebc62eeb3612e
James <5879831+CometDog@users.noreply.github.com>
2026-01-21 13:58:44 -0500

    Allow override of batch during batches request

--- src/ts/server/request/batches.ts
+++ src/ts/server/request/batches.ts
@@ -3,7 +3,11 @@ import { getServerLang } from "../localeManager";
 
 type BatchesResponse = { id: string; createdAt: string };
 
-export const batchesRequest = () =>
+export const batchesRequest = ({
+  batchId = "latest",
+}: {
+  batchId?: string;
+} = {}) =>
   fetchJSON<BatchesResponse>(
-    `https://news.kagi.com/api/batches/latest${getServerLang() && `?lang=${getServerLang()}`}`,
+    `https://news.kagi.com/api/batches/${batchId}${getServerLang() && `?lang=${getServerLang()}`}`,
   );
